5250a0cda194b759ddb6383b0745b6c0f5b0819f
Esteban Lescano <esteban.lescano@bitlogic.io>
2022-09-30 15:45:37 -0300

    commit 30-09

--- src/main/java/com/example/demo/maxikiosco/KioscoController.java
+++ src/main/java/com/example/demo/maxikiosco/KioscoController.java
@@ -1,5 +1,6 @@
 package com.example.demo.maxikiosco;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,26 +9,14 @@ import java.util.List;
 
 @RestController
 public class KioscoController {
+    private KioscoService service;
+    public KioscoController() {
+        this.service = new KioscoService();
+        service.loadCreate();
+    }
 
     @RequestMapping("/cobrar")
-    String cobrar(){
-        String nombre = "Carlitos";
-        String direccion = "Bithouse 234";
-        Golosina golosina1 = new Golosina(150,"alfajor","aguila");
-        Golosina golosina2 = new Golosina(75,"alfajor simple","tatin");
-        Golosina golosina3 = new Golosina(35,"alfajor simple","fulbito");
-
-        List<Golosina> listadoGolosinas = new ArrayList<>();
-        listadoGolosinas.add(golosina1);
-        listadoGolosinas.add(golosina2);
-        listadoGolosinas.add(golosina3);
-
-        Kiosco kiosco = new Kiosco( listadoGolosinas, nombre, direccion);
-
-        kiosco.cobrar(200);
-
-        return kiosco.toString();
-
+    String cobrar(@PathVariable List<String> idProducto){
 
     }
 }
